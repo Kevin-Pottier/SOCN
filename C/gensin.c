e7315b268adbf444f30d87eb63c743078e7be76f
Kevin Pottier <kevin.pottier@reseau.eseo.fr>
2024-12-04 10:47:27 +0100

    Changed main.c and compiled to print sinus values

--- C/gensin.c
+++ C/gensin.c
@@ -8,8 +8,16 @@
 // M_PI is defined in math.h
 
 int main(void) {
-
-
+	double nbpoints = SIN_PERIOD/STROBE_PERIOD;
+	int i;
+	
+	printf("nbpoints = %f\n", nbpoints);
+	printf("\n{\n");
+	
+	for(i=0;i<nbpoints; i++){
+		printf(" %d, ", (unsigned char) ((sin( (double) i*2.0*M_PI / nbpoints ) + 1) *255.0*2.0));
+	}
+	printf("\n}\n");
 
  return(0); 
 }
