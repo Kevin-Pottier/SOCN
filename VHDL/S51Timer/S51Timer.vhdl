0d810bf50a738d17c46d2b1953429974a956165e
Kevin Pottier <kevin.pottier@reseau.eseo.fr>
2025-01-04 18:22:37 +0100

    Implemented state machine in timer

--- VHDL/S51Timer/S51Timer.vhdl
+++ VHDL/S51Timer/S51Timer.vhdl
@@ -1,123 +1,125 @@
 
 Library IEEE;
- use IEEE.std_logic_1164.all;
- use IEEE.numeric_std.all;
+Use IEEE.std_logic_1164.All;
+Use IEEE.numeric_std.All;
 Library LibPkg;
- use LibPkg.MyUtils.all;
+Use LibPkg.MyUtils.All;
 Library LibPkgS51Cpu;
- use LibPkgS51Cpu.PkgS51Cpu.all;
+Use LibPkgS51Cpu.PkgS51Cpu.All;
 Library LibS51Timer;
- use LibS51Timer.PkgS51Timer.all;
- use LibS51Timer.PkgCompS51TimerCore.all;
+Use LibS51Timer.PkgS51Timer.All;
+Use LibS51Timer.PkgCompS51TimerCore.All;
 
-Entity S51Timer is
- Port(
-  RstB	        	: in    std_logic;
-  Ck            	: in    std_logic;
+Entity S51Timer Is
+  Port (
+    RstB : In STD_LOGIC;
+    Ck : In STD_LOGIC;
 
-  -- SFR INTERFACE
-  SfrDin		: in	std_logic_vector(PeriphDataBusSize -1 downto 0);
-  WriteSfr		: in	std_logic;
---  SfrDout		: out	std_logic_vector(PeriphDataBusSize -1 downto 0);
-  SfrDout_TSTAT		: out	std_logic_vector(PeriphDataBusSize -1 downto 0);
-  SfrDout_CNT_H		: out	std_logic_vector(PeriphDataBusSize -1 downto 0);
-  SfrDout_CNT_L		: out	std_logic_vector(PeriphDataBusSize -1 downto 0);
+    -- SFR INTERFACE
+    SfrDin : In STD_LOGIC_VECTOR(PeriphDataBusSize - 1 Downto 0);
+    WriteSfr : In STD_LOGIC;
+    --  SfrDout		: out	std_logic_vector(PeriphDataBusSize -1 downto 0);
+    SfrDout_TSTAT : Out STD_LOGIC_VECTOR(PeriphDataBusSize - 1 Downto 0);
+    SfrDout_CNT_H : Out STD_LOGIC_VECTOR(PeriphDataBusSize - 1 Downto 0);
+    SfrDout_CNT_L : Out STD_LOGIC_VECTOR(PeriphDataBusSize - 1 Downto 0);
 
---  ReadSfr		: in	std_logic;
-  
-  TCTRLWen 		: in	std_logic;
-  CNT_HWen		: in	std_logic;
-  CNT_LWen		: in	std_logic;
+    --  ReadSfr		: in	std_logic;
 
---  CNT_HRen		: in	std_logic;
---  CNT_LRen		: in	std_logic;
---  TSTATRen		: in	std_logic;
-  
--- PERIPHERALS
-  Time_out_IT		: out	std_logic;
-  timer_auto_idle	: out	std_logic
- );
+    TCTRLWen : In STD_LOGIC;
+    CNT_HWen : In STD_LOGIC;
+    CNT_LWen : In STD_LOGIC;
+
+    --  CNT_HRen		: in	std_logic;
+    --  CNT_LRen		: in	std_logic;
+    --  TSTATRen		: in	std_logic;
+
+    -- PERIPHERALS
+    Time_out_IT : Out STD_LOGIC;
+    timer_auto_idle : Out STD_LOGIC
+  );
 End S51Timer;
 
-Architecture rtl of S51Timer is
+Architecture rtl Of S51Timer Is
 
--- Interface regs
- signal CNT_H			: std_logic_vector(7 downto 0);
- signal CNT_L			: std_logic_vector(7 downto 0);
- signal ROLLV			: std_logic;
- signal TAI			: std_logic;
- signal autoidle		: std_logic;
+  -- Interface regs
+  Signal CNT_H : STD_LOGIC_VECTOR(7 Downto 0);
+  Signal CNT_L : STD_LOGIC_VECTOR(7 Downto 0);
+  Signal ROLLV : STD_LOGIC;
+  Signal TAI : STD_LOGIC;
+  Signal autoidle : STD_LOGIC;
 
--- Internal control/cfg
- signal start			: std_logic;
- signal stop			: std_logic;
- signal clrstat			: std_logic;
- signal cnt			: std_logic_vector(15 downto 0);
- signal reload			: std_logic;
+  -- Internal control/cfg
+  Signal start : STD_LOGIC;
+  Signal stop : STD_LOGIC;
+  Signal clrstat : STD_LOGIC;
+  Signal cnt : STD_LOGIC_VECTOR(15 Downto 0);
+  Signal reload : STD_LOGIC;
 
-begin
+Begin
 
--- process(RstB, Ck)
--- begin
---  if RstB = '0' then
---   CNT_H <= 
---   CNT_L <= 
---  elsif Ck'event and Ck = '1' then
---
---
---
---  end if;  
--- end process;
---
--- process(RstB, Ck)
--- begin
---  if RstB = '0' then
---   ROLLV <= 
---  elsif Ck'event and Ck = '1' then
---
---  end if;  
--- end process;
---
--- process(RstB, Ck)
--- begin
---  if RstB = '0' then
---   TAI <= 
---  elsif Ck'event and Ck = '1' then
---
---  end if;  
--- end process;
---
---
+  Process (RstB, Ck)
+  Begin
+    If RstB = '0' Then
+      CNT_H <= (Others => '0'); -- reset init
+      CNT_L <= (Others => '0'); -- reset init
+    Elsif Ck'event And Ck = '1' Then
+      If CNT_HWen = '1' Then
+        CNT_H <= SfrDin(7 Downto 0); -- Write in CNT_H
+      End If;
+      If CNT_LWen = '1' Then
+        CNT_L <= SfrDin(7 Downto 0); -- Write in CNT_L
+      End If;
+    End If;
+  End Process;
 
-SfrDout_TSTAT <= "0000000"&ROLLV;
-SfrDout_CNT_H <= CNT_H;
-SfrDout_CNT_L <= CNT_L;
+  Process (RstB, Ck)
+  Begin
+    If RstB = '0' Then
+      ROLLV <= '0'; -- Reset
+    Elsif Ck'event And Ck = '1' Then
+      If reload = '1' Then
+        ROLLV <= '1'; -- Timer reached its end
+      Elsif clrstat = '1' Then
+        ROLLV <= '0'; -- Manual reset
+      End If;
+    End If;
+  End Process;
 
---   SfrDout <= 	"0000000"&ROLLV	when ReadSfr = '1' and TSTATRen = '1' else
---    		CNT_H		when ReadSfr = '1' and CNT_HREn = '1' else
---     		CNT_L		when ReadSfr = '1' and CNT_LREn = '1' else
---		(others => 'Z');
---
--- cnt <= CNT_H & CNT_L;
--- start <= WriteSfr and TCTRLWen and SfrDin(0);
--- stop <= 
--- clrstat <= 
--- autoidle <= 
---
--- S51TimerCore_I : S51TimerCore
--- Port map(
---  RstB			=> RstB,        
---  Ck			=> Ck,
---  
---  start			=> start,
---  stop			=> stop,
---  cnt			=> cnt,
---  reload		=> reload
--- );
--- 
+  -- process(RstB, Ck)
+  -- begin
+  --  if RstB = '0' then
+  --   TAI <= 
+  --  elsif Ck'event and Ck = '1' then
+  --
+  --  end if;  
+  -- end process;
 
- timer_auto_idle <= '0';
- Time_out_IT <= '0';
- 
+  SfrDout_TSTAT <= "0000000" & ROLLV;
+  SfrDout_CNT_H <= CNT_H;
+  SfrDout_CNT_L <= CNT_L;
+
+  --   SfrDout <= 	"0000000"&ROLLV	when ReadSfr = '1' and TSTATRen = '1' else
+  --    		CNT_H		when ReadSfr = '1' and CNT_HREn = '1' else
+  --     		CNT_L		when ReadSfr = '1' and CNT_LREn = '1' else
+  --		(others => 'Z');
+
+  cnt <= CNT_H & CNT_L;
+  start <= WriteSfr And TCTRLWen And SfrDin(0);
+  stop <= WriteSfr And TCTRLWen And SfrDin(1);
+  clrstat <= WriteSfr And TCTRLWen And SfrDin(2);
+  -- autoidle <= WriteSfr and TCTRLWen and SfrDin(3);
+
+  S51TimerCore_I : S51TimerCore
+  Port Map(
+    RstB => RstB,
+    Ck => Ck,
+
+    start => start,
+    stop => stop,
+    cnt => cnt,
+    reload => reload
+  );
+  timer_auto_idle <= '0';
+  Time_out_IT <= '0';
+
 End rtl;
-
